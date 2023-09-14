78cca684380ca93296f6a939121317799305e609
Nguyen Thanh Nam <nn5591078@gmail.com>
2023-09-14 17:38:32 +0700

    them file README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Nội dung bảng kế hoạch thực hiện:
+## Thông tin sinh viên:
+Họ và tên: Nguyễn Thành Nam
+Mã SV: 2121110322
+Lớp: CCQ2111J
+Email: nn5591078@gmail.com
+Đề tài: Website bán quần áo
+## Ngôn ngữ:
++JavaScript
++ReactJS
+## Thời hạn deadline:
+Từ ngày 7/9 -> 14/9 : làm giao diện web
--- myweb/src/App.js
+++ myweb/src/App.js
@@ -1,22 +1,23 @@
 import "../src/assets/sass/app.scss";
-import Copyright from "./layouts/Copyright";
 
-import Footer from "./layouts/Footer";
-import Header from "./layouts/header";
-import Main from "./layouts/Main";
-import { BrowserRouter } from "react-router-dom";
+import Product_detail from "./pages/Product-detail";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
     
     <>
-    
+      
       <BrowserRouter>
-        <Header />
-        <Main />
-        <Footer />
-        <Copyright/>
+      <Routes>
+        <Route path="/" element={<LayoutSite />}> 
+        {/* router có thẻ đóng và mở là thằng cha thằng con sẻ kế thừa thằng cha :V */}
+          <Route index element={<Home />} />
+          <Route path="product-detail" element={<Product_detail />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
       </BrowserRouter>
 
 
